8dc1cde5b47776653b4aa7d1c208124028e72f16
Aftab48 <mdalam4884@gmail.com>
2023-11-21 12:58:00 +0530

    teamslide commit

--- client/src/utils/teams.js
+++ client/src/utils/teams.js
@@ -1,18 +1,18 @@
 export const teamSlide = {
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 50,
     breakpoints: {
-        480: {
-            slidesPerView: 2,
-        },
-        600: {
-            slidesPerView: 2,
-        },
-        750: {
-            slidesPerView:2,
-        },
-        1100: {
-            slidesPerView: 2,
-        }
-    }
-}
+      480: {
+        slidesPerView: 1,
+      },
+      600: {
+        slidesPerView: 1,
+      },
+      750: {
+        slidesPerView: 1,
+      },
+      1100: {
+        slidesPerView: 2,
+      },
+    },
+  };
