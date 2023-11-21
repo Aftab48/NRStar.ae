24d54ea7412810a310d26427dab74195007c8307
Aftab48 <mdalam4884@gmail.com>
2023-11-21 10:38:38 +0530

    auth0 update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-nrstar.uk.auth0.com"
      clientId="JiZK6rfKVzfxGSUXkhrJDydU5kNVepmW"
      authorizationParams={{
-      redirect_uri: "http://localhost:5173"
+      redirect_uri: "https://nr-star-ae.vercel.app"
      }}
      audience="http://localhost:8000"
      scope="openid profile email"
